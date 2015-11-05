0e5e2937ca7805f5f7bf5fbb02cf1d70d5caae4f
Chara Tsoukala <charatsoukala@160-184.wlan-int.ru.nl>
2015-11-05 11:23:06 +0100

    dual path image

--- dual_path.py
+++ dual_path.py
@@ -1,4 +1,5 @@
 import math
+#import numpy as np
 
 # lexicon_str = "dog cat chases bites the a is are"
 # lexicon = lexicon_str.split()
@@ -32,27 +33,32 @@ extra_messages_lab = ["THROW(BOY,BOX,DOG)",
                       "SHOW(GIRL,BALL,BOY)",
                       "PUSH(DOG, BALL, BOX)"]'''
 
-# input layers
+# 4 input layers
 event_sem_size = len(event_sem)
 lexicon_size = len(lexicon)
 concept_size = len(concepts)
 roles_size = len(roles)
 
-# Hidden layers, values taken from dualpath3.in 
+# Hidden layers (context and hidden), values taken from dualpath3.in
+# compress is also hidden
 hidden_size = 20
 context_size = hidden_size
 compress_size = 10 # is it coincidence that it's hidden/2?
 # I guess compress_size and c_compress_size need to have the same size
-c_compress_size = compress_size 
-
-#sent_pos
-#int time
-#direction = 2
-#mode = 0
-#mode_type = 0
-#learn_level = 300
+c_compress_size = compress_size
+'''sent_pos
+int time
+direction = 2
+mode = 0
+mode_type = 0
+learn_level = 300'''
 # check the paper to see what the learning rate is
-learning_rate = 0.1
+learn_rate = 0.1
+
 
 def sigmoid(x):
-  return 1 / (1 + math.exp(-x))
+    return 1 / (1 + math.exp(-x))
+
+
+def sigmoid_prime(x):
+    return sigmoid(x)*(1-sigmoid(x))
