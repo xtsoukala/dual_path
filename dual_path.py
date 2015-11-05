6b04086cc33901a465d0c4633bc610925c4cbb8f
Chara Tsoukala <charatsoukala@026-037.wlan-ext.ru.nl>
2015-11-05 17:18:27 +0100

    more layers

--- dual_path.py
+++ dual_path.py
@@ -35,23 +35,43 @@ extra_messages_lab = ["THROW(BOY,BOX,DOG)",
 
 # 4 input layers
 event_sem_size = len(event_sem)
+
 lexicon_size = len(lexicon)
 concept_size = len(concepts)
 roles_size = len(roles)
+# same for c
+c_lexicon_size = lexicon_size
+c_concept_size = concept_size
+c_roles_size = roles_size
 
-# Hidden layers (context and hidden), values taken from dualpath3.in
-# compress is also hidden
-hidden_size = 20
-context_size = hidden_size
-compress_size = 10 # is it coincidence that it's hidden/2?
-# I guess compress_size and c_compress_size need to have the same size
-c_compress_size = compress_size
+''' http://tedlab.mit.edu/~dr/Lens/Commands/addGroup.html
+c_lexicon_size: addGroup cword $lexSize ELMAN ELMAN_CLAMP ELMAN_CLAMP -BIASED OUT_NORM
+c_compress_size: addGroup ccompress $ccompressSize -BIASED
+c_concept_size: addGroup cwhat $semSize OUTPUT TARGET_COPY -BIASED -WRITE_OUTPUTS
+c_roles_size: addGroup cwhere2 $whereSize ELMAN ELMAN_CLAMP ELMAN_CLAMP -BIASED
+[why 2?] addGroup cwhere $whereSize SOFT_MAX -BIASED
+event_sem_size: addGroup eventsem $eventsemSize LINEAR -BIASED
+context_size: addGroup context $contextSize ELMAN OUT_INTEGR -BIASED
+hidden_size: addGroup hidden $hiddenSize -BIASED
+role_size: addGroup where $whereSize -BIASED
+concept_size: addGroup what $semSize -BIASED
+compress_size: addGroup compress $compressSize -BIASED
+(how do cword and targ differ?) : addGroup targ $lexSize INPUT
+lexicon_size: addGroup word $lexSize OUTPUT SOFT_MAX STANDARD_CRIT USE_OUTPUT_HIST USE_TARGET_HIST -BIASED'''
 '''sent_pos
 int time
 direction = 2
 mode = 0
 mode_type = 0
 learn_level = 300'''
+
+# Hidden layers (context and hidden), values taken from dualpath3.in
+# compress is also hidden
+hidden_size = 20
+context_size = hidden_size
+compress_size = 10  # is it coincidence that it's hidden/2?
+c_compress_size = compress_size
+
 # check the paper to see what the learning rate is
 learn_rate = 0.1
 
